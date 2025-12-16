460df84d59b5f94694fedcee91a7ba5febf88062
Heet Patel <heetkpatel30@gmail.com>
2025-12-16 18:07:18 +0000

    Initial Commit

---
+++ NextUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextUp
+//
+//  Created by Heet Patel on 16/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
