4879426a260379783143c3d89b8e464e5e3ce2f0
Heet Patel <heetkpatel30@gmail.com>
2025-12-16 23:47:54 +0000

    feat: enums for watch status and media types for use in the other models

---
+++ Data/Models/MediaType.swift
@@ -0,0 +1,12 @@
+//
+//  MediaType.swift
+//  NextUp
+//
+//  Created by Heet Patel on 16/12/2025.
+//
+
+import Foundation
+
+enum MediaType: String, Codable {
+    case movie, tvShow
+}
---
+++ Data/Models/WatchStatus.swift
@@ -0,0 +1,12 @@
+//
+//  WatchStatus.swift
+//  NextUp
+//
+//  Created by Heet Patel on 16/12/2025.
+//
+
+import Foundation
+
+enum WatchStatus: String, Codable {
+    case watching, completed, paused, abandoned
+}
